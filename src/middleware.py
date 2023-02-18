1c0752d507d29cd9df68102c08faf668ec8b33ae
Utkarsh Jain <67118602+QuantumStatic@users.noreply.github.com>
2023-02-18 20:34:39 +0800

    Fixed middelware

--- src/middleware.py
+++ src/middleware.py
@@ -8,8 +8,13 @@ CORS(app)
 
 @app.route("/sendText/<text>", methods=["GET"])
 def hello(text):
-    print(f"We got {text}")
-    return {'body':json.dumps(f"We got {text}")}
+    try:
+        response = flask.make_response(f"{text}")
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
+    except Exception as e:
+        return flask.make_response(f"{e.__class__.__name__}: {e}")
+    
 
 @app.route("/sendText", methods=["OPTIONS", "POST"])
 def recieve_text_post():
